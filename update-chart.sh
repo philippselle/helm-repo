8f357820cc70723ef800ddf47f0b1bf6ee62a199
sellep <sellep@de.ibm.com>
2022-03-31 18:36:23 +0200

    update Helm Chart

---
+++ update-chart.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+VERSION=$(grep 'version:' helm-chart-sources/pipeline/Chart.yaml | cut -d ":" -f2)
+NEXTVERSION=$(echo ${VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
+
+sed -i '' 's,'"$VERSION"', '"$NEXTVERSION"',' helm-chart-sources/pipeline/Chart.yaml 
+
+helm package helm-chart-sources/pipeline
+
+helm repo index --url https://philippselle.github.io/helm-repo/ --merge index.yaml .
+
+git add .
+
+git commit -m "update Helm Chart"
+
+git push
